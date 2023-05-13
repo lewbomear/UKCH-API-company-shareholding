b4fb07d90520f8cbcfc3ce75a7875442cb85d4d5
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-05-13 18:07:30 +0100

    reformat

--- src/generate_relevant_individual_info.py
+++ src/generate_relevant_individual_info.py
@@ -19,23 +19,20 @@ def generate_relevant_individual_info():
     officer_name = os.environ.get("OFFICER_NAME")
     officer_dob = os.environ.get("OFFICER_DOB")
 
-
-
-    url = f"https://api.company-information.service.gov.uk/search/officers?q=\"{officer_name}\""
-
+    url = f'https://api.company-information.service.gov.uk/search/officers?q="{officer_name}"'
 
     headers = {"Authorization": api_key}
 
     # Add a title to the document
     document.add_heading(f"Associated companies for: {officer_name}", level=1)
-    current_heading = document.add_heading('Current appointments', level=1)
-    current_end = document.add_paragraph('-')
-    former_heading = document.add_heading('Former appointments', level=1)
-    former_end = document.add_paragraph('--')
+    current_heading = document.add_heading("Current appointments", level=1)
+    current_end = document.add_paragraph("-")
+    former_heading = document.add_heading("Former appointments", level=1)
+    former_end = document.add_paragraph("--")
 
     # Load the SIC codes and descriptions from the CSV file
-    with open("SIC07_CH_condensed_list_en.csv", newline='') as csvfile:
-        sic_codes_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    with open("SIC07_CH_condensed_list_en.csv", newline="") as csvfile:
+        sic_codes_reader = csv.reader(csvfile, delimiter=",", quotechar='"')
         sic_mapping = {row[0]: row[1] for row in sic_codes_reader}
 
     # Make the API request
@@ -44,13 +41,20 @@ def generate_relevant_individual_info():
 
     # only keep officers with exact name match
     exact_name_matches = []
-    for item in data['items']:
-        if item['title'].lower() == officer_name.lower():
+    for item in data["items"]:
+        if item["title"].lower() == officer_name.lower():
             exact_name_matches.append(item)
 
     exact_name_dob_matches = []
-    for item in data['items']:
-        if item['title'].lower() == officer_name.lower() and isinstance(item.get('date_of_birth'), dict) and str(item['date_of_birth']['year'])+'-'+str(item['date_of_birth']['month']).zfill(2) == officer_dob[:7]:
+    for item in data["items"]:
+        if (
+            item["title"].lower() == officer_name.lower()
+            and isinstance(item.get("date_of_birth"), dict)
+            and str(item["date_of_birth"]["year"])
+            + "-"
+            + str(item["date_of_birth"]["month"]).zfill(2)
+            == officer_dob[:7]
+        ):
             exact_name_dob_matches.append(item)
 
     # Loop through the exact name matches and make a request to each URL in the links dictionary
@@ -64,56 +68,70 @@ def generate_relevant_individual_info():
         while remaining_results > 0:
             officer_url = f"https://api.company-information.service.gov.uk{match['links']['self']}?page={page_no}&items_per_page={items_per_page}&start_index={start_index}"
             officer_response = requests.get(officer_url, headers=headers)
-            current_data = officer_response.json() #gets the current data
+            current_data = officer_response.json()  # gets the current data
 
-            with open(f'{officer_name} page {page_no}.json', 'w', encoding='utf-8') as json_file: #open a files to dump the data named by the page
-                json.dump(current_data, json_file,  ensure_ascii=False, indent=4)
+            with open(
+                f"{officer_name} page {page_no}.json", "w", encoding="utf-8"
+            ) as json_file:  # open a files to dump the data named by the page
+                json.dump(current_data, json_file, ensure_ascii=False, indent=4)
             # officer_data_cache.append(current_data) #adds it to a big cache
 
-            if total_results < 0: #if you haven't checked the total results
-                total_results = current_data['total_results'] #it will fetch the number
-                remaining_results = total_results # and set the remaining results to total
-            remaining_results -= items_per_page #it then removes the ones from the last set
-            start_index += items_per_page #increases the start index to that number
-            if remaining_results < items_per_page: #then if there's less results left
-                items_per_page = remaining_results #set as remainder
-            page_no +=1 #increase page count
-            
+            if total_results < 0:  # if you haven't checked the total results
+                total_results = current_data[
+                    "total_results"
+                ]  # it will fetch the number
+                remaining_results = (
+                    total_results  # and set the remaining results to total
+                )
+            remaining_results -= (
+                items_per_page  # it then removes the ones from the last set
+            )
+            start_index += items_per_page  # increases the start index to that number
+            if remaining_results < items_per_page:  # then if there's less results left
+                items_per_page = remaining_results  # set as remainder
+            page_no += 1  # increase page count
+
         officer_data = {}
 
         for current_page in range(1, page_no, 1):
-            with open(f'{officer_name} page {current_page}.json', encoding='utf-8') as json_file:
+            with open(
+                f"{officer_name} page {current_page}.json", encoding="utf-8"
+            ) as json_file:
                 for line in json_file:
                     converted_data = json.loads(line)
-                    merged[converted_data['items']].update(converted_data) # asuming both file has same ids otherwise use try catch
+                    merged[converted_data["items"]].update(
+                        converted_data
+                    )  # asuming both file has same ids otherwise use try catch
 
         merged = list(merged.values())
         print(merged)
 
         # Loop through the officer's appointments and print the company name, number, and nature of business
-        for appointment in officer_data['items']:
-            company_name = appointment['appointed_to']['company_name']
-            company_number = appointment['appointed_to']['company_number']
-            appointed_on = appointment['appointed_on']
-            officer_role = appointment['officer_role']
-            company_status = appointment['appointed_to']['company_status']
-            psc_name = ''
+        for appointment in officer_data["items"]:
+            company_name = appointment["appointed_to"]["company_name"]
+            company_number = appointment["appointed_to"]["company_number"]
+            appointed_on = appointment["appointed_on"]
+            officer_role = appointment["officer_role"]
+            company_status = appointment["appointed_to"]["company_status"]
+            psc_name = ""
             company_title = f"{company_name} ({company_number})"
 
             # Get the company profile URL
             company_profile_url = f"https://api.company-information.service.gov.uk/company/{company_number}"
-            company_profile_response = requests.get(company_profile_url, headers=headers)
+            company_profile_response = requests.get(
+                company_profile_url, headers=headers
+            )
             company_profile_data = company_profile_response.json()
 
             # Get the SIC code and look up the activity in the mapping dictionary
-            sic_code = company_profile_data.get('sic_codes', ["N/A"])[0]
+            sic_code = company_profile_data.get("sic_codes", ["N/A"])[0]
             activity = sic_mapping.get(sic_code, "Unknown")
 
-            #Get company status, incorporation date, dissolution date
+            # Get company status, incorporation date, dissolution date
 
-            company_inc = company_profile_data.get('date_of_creation')
-            if 'date_of_cessation' in company_profile_data:
-                company_dis = company_profile_data['date_of_cessation']
+            company_inc = company_profile_data.get("date_of_creation")
+            if "date_of_cessation" in company_profile_data:
+                company_dis = company_profile_data["date_of_cessation"]
             else:
                 company_dis = None
 
@@ -122,14 +140,14 @@ def generate_relevant_individual_info():
             company_psc_response = requests.get(company_psc_url, headers=headers)
             company_psc_data = company_psc_response.json()
 
-            #print(json.dumps(company_psc_data, indent=4))
+            # print(json.dumps(company_psc_data, indent=4))
 
             # Loop through the company's persons with significant control and print the name
             psc_names = []
-            if 'items' in company_psc_data and company_psc_data['items']:
-                for psc in company_psc_data['items']:
-                    if 'name' in psc:
-                        psc_name = psc['name']
+            if "items" in company_psc_data and company_psc_data["items"]:
+                for psc in company_psc_data["items"]:
+                    if "name" in psc:
+                        psc_name = psc["name"]
                         psc_names.append(psc_name)
             else:
                 print("No PSC data available for this company.")
@@ -140,32 +158,33 @@ def generate_relevant_individual_info():
                     for psc_name in psc_names:
                         psc_statement = f"The company has a person with significant control named {psc_name}."
                     new_paragraph = former_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} was appointed {officer_role} of {company_name} ({company_number}) on {appointed_on} and resigned on {resigned_on}. The nature of business is {activity}. {psc_statement}")
-
+                        f"{company_name} ({company_number}) \n{officer_name} was appointed {officer_role} of {company_name} ({company_number}) on {appointed_on} and resigned on {resigned_on}. The nature of business is {activity}. {psc_statement}"
+                    )
 
                 elif len(psc_names) > 1:
                     last_name = psc_names.pop()
                     full_list = ", ".join(psc_names)
                     psc_statement = f"The company has the following persons with significant control: {full_list} and {last_name}."
                     new_paragraph = former_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} was appointed {officer_role} of {company_name} ({company_number}) on {appointed_on} and resigned on {resigned_on}. The nature of business is {activity}. {psc_statement}")
-
+                        f"{company_name} ({company_number}) \n{officer_name} was appointed {officer_role} of {company_name} ({company_number}) on {appointed_on} and resigned on {resigned_on}. The nature of business is {activity}. {psc_statement}"
+                    )
 
                 else:
                     psc_statement = (
                         "The company has no persons with significant control"
                     )
                     new_paragraph = former_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} was appointed {officer_role} of {company_name} on {appointed_on} and resigned on {resigned_on}. The nature of business is {activity}. {psc_statement}")
+                        f"{company_name} ({company_number}) \n{officer_name} was appointed {officer_role} of {company_name} on {appointed_on} and resigned on {resigned_on}. The nature of business is {activity}. {psc_statement}"
+                    )
 
             else:
                 if len(psc_names) == 1:
                     for psc_name in psc_names:
                         psc_statement = f"The company has a person with significant control named {psc_name}."
-                        if 'active' in company_status:
+                        if "active" in company_status:
                             new_paragraph = current_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} has been serving as {officer_role} of {company_name} since {appointed_on}. The nature of business is {activity}. {psc_statement}"
-                    )
+                                f"{company_name} ({company_number}) \n{officer_name} has been serving as {officer_role} of {company_name} since {appointed_on}. The nature of business is {activity}. {psc_statement}"
+                            )
                         else:
                             new_paragraph = former_end.insert_paragraph_before(
                                 f"{company_name} ({company_number}) \n{officer_name} served as {officer_role} of {company_name} between {appointed_on} and {company_dis}. The nature of business was {activity}. {psc_statement}"
@@ -174,31 +193,32 @@ def generate_relevant_individual_info():
                     last_name = psc_names.pop()
                     full_list = ", ".join(psc_names)
                     psc_statement = f"The company has the following persons with significant control: {full_list} and {last_name}."
-                    if 'active' in company_status:
+                    if "active" in company_status:
                         new_paragraph = current_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} has been serving as {officer_role} of {company_name} since {appointed_on}. The nature of business is {activity}. {psc_statement}"
-                    )
+                            f"{company_name} ({company_number}) \n{officer_name} has been serving as {officer_role} of {company_name} since {appointed_on}. The nature of business is {activity}. {psc_statement}"
+                        )
                     else:
                         new_paragraph = former_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} served as {officer_role} of {company_name} between {appointed_on} and {company_dis}. The nature of business is {activity}. {psc_statement}"
+                            f"{company_name} ({company_number}) \n{officer_name} served as {officer_role} of {company_name} between {appointed_on} and {company_dis}. The nature of business is {activity}. {psc_statement}"
                         )
 
                 else:
                     psc_statement = (
                         "The company has no persons with significant control"
                     )
-                    if 'active' in company_status:
+                    if "active" in company_status:
                         new_paragraph = current_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} has been serving as {officer_role} of {company_name} since {appointed_on}. The nature of business is {activity}. {psc_statement}"
-                    )
+                            f"{company_name} ({company_number}) \n{officer_name} has been serving as {officer_role} of {company_name} since {appointed_on}. The nature of business is {activity}. {psc_statement}"
+                        )
                     else:
                         new_paragraph = former_end.insert_paragraph_before(
-                        f"{company_name} ({company_number}) \n{officer_name} served as {officer_role} of {company_name} between {appointed_on} and {company_dis}. The nature of business was {activity}. {psc_statement}"
+                            f"{company_name} ({company_number}) \n{officer_name} served as {officer_role} of {company_name} between {appointed_on} and {company_dis}. The nature of business was {activity}. {psc_statement}"
                         )
     # Save the document as a Word file
-    document.save(f'Associated companies for {officer_name}.docx')
+    document.save(f"Associated companies for {officer_name}.docx")
     document_text = docx.Document(f"Associated companies for {officer_name}.docx")
     print(document_text)
+
 
 if __name__ == "__main__":
     generate_relevant_individual_info()
--- src/print_shareholder_info.py
+++ src/print_shareholder_info.py
@@ -22,23 +22,27 @@ def print_shareholder_info():
     # Make a filing history request to get all confirmation statements
     url = f"https://api.company-information.service.gov.uk/company/{company_number}/filing-history?category=confirmation-statement&items_per_page=100"
 
-    response = requests.get(url, auth=(api_key, ''))
+    response = requests.get(url, auth=(api_key, ""))
 
     # Parse the response for the link to the document metadata
     response_json = response.json()
-    items = response_json['items']
+    items = response_json["items"]
 
     # Check each confirmation statement to only select the latest confirmation statement with updates
     for item in items:
-        description = item['description']
-        if 'confirmation-statement-with-updates' in description.lower():
-            metadata_link = item['links']['document_metadata']
-            metadata_response = requests.get(metadata_link, auth=(api_key, ''), headers={'Accept': 'application/json'})
+        description = item["description"]
+        if "confirmation-statement-with-updates" in description.lower():
+            metadata_link = item["links"]["document_metadata"]
+            metadata_response = requests.get(
+                metadata_link,
+                auth=(api_key, ""),
+                headers={"Accept": "application/json"},
+            )
             metadata_response_json = metadata_response.json()
-            document_link = metadata_response_json['links']['document']
+            document_link = metadata_response_json["links"]["document"]
 
             # Get the content of the document
-            document_response = requests.get(document_link, auth=(api_key, ''))
+            document_response = requests.get(document_link, auth=(api_key, ""))
             document_content = document_response.content
 
             # Write the content to a file named "confirmation_statement.pdf"
@@ -46,37 +50,55 @@ def print_shareholder_info():
                 f.write(document_content)
             break
 
-    pages = convert_from_path('confirmation_statement.pdf', 250, poppler_path=r'C:\Program Files\poppler-23.01.0\Library\bin')
+    pages = convert_from_path(
+        "confirmation_statement.pdf",
+        250,
+        poppler_path=r"C:\Program Files\poppler-23.01.0\Library\bin",
+    )
 
     images = []
     for page in pages:
         images.append(page)
 
     # Combine all the images into one
-    combined_image = Image.new('RGB', (images[0].width, sum([i.height for i in images])))
+    combined_image = Image.new(
+        "RGB", (images[0].width, sum([i.height for i in images]))
+    )
     y_offset = 0
     for image in images:
         combined_image.paste(image, (0, y_offset))
         y_offset += image.height
 
     # Save the combined image (note for later: see if i can save the image as a temp file)
-    combined_image.save('confirmation_statement.png', 'PNG')
+    combined_image.save("confirmation_statement.png", "PNG")
 
     # Set the path to Tesseract
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    )
 
     # Load the image
-    combined_image = Image.open('confirmation_statement.png')
+    combined_image = Image.open("confirmation_statement.png")
 
     # Use pytesseract to extract text from the image and format it into one paragraph, while limiting instances of three spaces to two
-    text = pytesseract.image_to_string(combined_image).replace('\n\n', '\n').replace('\n', '  ').replace('confirmation  statement', 'confirmation statement').replace('this  confirmation', 'this confirmation').replace('of  this confirmation', 'of this confirmation').replace('date  of this', 'date of this').replace(r'\b([A-Z][a-z]+)  ([A-Z][a-z]+)\b', r'\1 \2')
-
-
+    text = (
+        pytesseract.image_to_string(combined_image)
+        .replace("\n\n", "\n")
+        .replace("\n", "  ")
+        .replace("confirmation  statement", "confirmation statement")
+        .replace("this  confirmation", "this confirmation")
+        .replace("of  this confirmation", "of this confirmation")
+        .replace("date  of this", "date of this")
+        .replace(r"\b([A-Z][a-z]+)  ([A-Z][a-z]+)\b", r"\1 \2")
+    )
 
     print(text)
 
     # Find all instances of "(?<!0 )(\d+) ORDINARY shares held as at the date of this confirmation statement" and the name that follows
-    shares = re.findall(r'(?<!0 )(\d+) ORDINARY shares held as at the date of this confirmation statement  Name: (\S+(?: \S+)*)', text)
+    shares = re.findall(
+        r"(?<!0 )(\d+) ORDINARY shares held as at the date of this confirmation statement  Name: (\S+(?: \S+)*)",
+        text,
+    )
 
     # Calculate the total number of shares
     total_shares = sum([int(share[0]) for share in shares])
